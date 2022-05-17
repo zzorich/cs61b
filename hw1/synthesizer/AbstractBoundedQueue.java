67d818a01d6e79c9a9b9a71b2fa663c53d28e108
lingji zhou <lingji.zhou17@gmail.com>
2022-05-17 23:18:22 +0100

    HW1 completed

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -11,14 +11,4 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     public int fillCount() {
         return this.fillCount;
     }
-
-    @Override
-    public boolean isEmpty() {
-        return this.fillCount == 0;
-    }
-
-    @Override
-    public boolean isFull() {
-        return this.fillCount == this.capacity;
-    }
 }
