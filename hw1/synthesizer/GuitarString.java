66f33711d930e227ae81de3a4486bc898b78868e
lingji zhou <lingji.zhou17@gmail.com>
2022-05-17 23:37:52 +0100

    HW1 completed

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -15,6 +15,9 @@ public class GuitarString {
     public GuitarString(double frequency) {
         int capacity = (int) Math.round(SR / frequency);
         buffer = new ArrayRingBuffer<>(capacity);
+        for (int i = 0; i < capacity; i++) {
+            buffer.enqueue(0.0);
+        }
     }
 
 
