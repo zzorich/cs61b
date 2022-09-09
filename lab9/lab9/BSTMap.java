98c15b9d4d422bba4a458367dd93150ccaace121
lingji zhou <lingji.zhou17@gmail.com>
2022-09-09 01:44:25 +0100

    good

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -161,6 +161,9 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, Iterabl
             rt.right = removeHelper(rt.right, key);
             return rt;
         }
+        if (rt.right == null) {
+            return rt.left;
+        }
         Node smallestNode = min(rt.right);
         smallestNode.right = removeMin(rt.right); //size has been reduced here, no further reduction is needed.
         smallestNode.left = rt.left;
